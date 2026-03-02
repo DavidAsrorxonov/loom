7907439ce15a98b84256ed8bba98d192276d1f95
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-02 22:02:55 +0900

    feat: Add state and ref hooks for screen recording management

--- components/screen-recorder.tsx
+++ components/screen-recorder.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+
 const ScreenRecorder = () => {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [mediaBlob, setMediaBlob] = useState<Blob | null>(null);
+
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const chunksRef = useRef<Blob[]>([]);
+  const screenStreamRef = useRef<MediaStream | null>(null);
+  const micStreamRef = useRef<MediaStream | null>(null);
+  const liveVideoRef = useRef<HTMLVideoElement>(null);
+
+  const router = useRouter();
+
   return <div>ScreenRecorder</div>;
 };
 
